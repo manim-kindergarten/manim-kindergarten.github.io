2f88d1e26b9c9652a348c6b0b377879088cdeadd
widcardw <55699713+widcardw@users.noreply.github.com>
2022-10-28 12:58:09 +0000

    Deploying to gh-pages from @ manim-kindergarten/manim-kindergarten.github.io@8e0455ceb17023423857e7655e32052339a61a69 🚀

---
+++ assets/404.3358d18c.js
@@ -0,0 +1,1 @@
+import{d as e,a as o,o as a,c as s,t as c,f as r,i as n}from"./app.c8e3fd58.js";const f=e({__name:"404",setup(i){const{t}=o();return(p,u)=>(a(),s("div",null,c(r(t)("not-found")),1))}});typeof n=="function"&&n(f);export{f as default};
